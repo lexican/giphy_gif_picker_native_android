09b0510ca76083ec0527c61812d78118f62adae9
Adeyemo Adekunle <adekunle@nestcoin.com>
2022-12-17 22:13:55 +0100

    initial commit

---
+++ app/src/main/java/com/kunlexze/giphy_gif_picker_native_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kunlexze.giphy_gif_picker_native_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
